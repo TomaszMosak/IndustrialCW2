103571a91a5f244a57f9dcb8f7a4cfc5b5785e5d
Tomasz Mosak <46677243+TomaszMosak@users.noreply.github.com>
2019-11-29 17:37:17 +0000

    Task 2 complete

--- IndustrialCW2/main.py
+++ IndustrialCW2/main.py
@@ -1,7 +1,9 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import json
 import sys
 import re
+import pycountry
+from pycountry_convert import country_alpha2_to_continent_code, country_name_to_country_alpha2
 
 userData = dict()
 userCountryCode = dict()
@@ -24,14 +26,73 @@ def readJSON(fileLocation):
             userData[i] = currUser
             i += 1
 
+    print("The user dictionary has been successfully populated")
+
+#TASK2A START
+def countryCount(documentID):
+    for country in range(0, len(userData)):
+        if 'visitor_country' in userData[country] and 'env_doc_id' in userData[country]:
+            if userData[country]['env_doc_id'] == documentID:
+                dictKey = userData[country]['visitor_country']
+                if dictKey in userCountryCode:
+                    userCountryCode[dictKey] += 1
+                else:
+                    userCountryCode[dictKey] = 1
 
 
+def countryPrint():
+    countries = list(userCountryCode.keys())
+    plt.bar(countries, height=list(userCountryCode.values()))
+    plt.xticks(countries, countries)
+    plt.show()
+
+#TASK2A END
+
+#TASK2B START
+def continentCount(documentID):
+    continents = {
+    'NA': 'North America',
+    'SA': 'South America', 
+    'AS': 'Asia',
+    'OC': 'Australia',
+    'AF': 'Africa',
+    'EU': 'Europe'
+}
+    for country in range(0, len(userData)):
+            continent = continents[country_alpha2_to_continent_code(userData[country]['visitor_country'])]
+            if userData[country]['env_doc_id'] == documentID:
+                dictKey = continent
+                if dictKey in userContinentCode:
+                    userContinentCode[dictKey] += 1
+                else:
+                    userContinentCode[dictKey] = 1
+        
+def continentPrint():
+    continents = list(userContinentCode.keys())
+    plt.bar(continents, height=list(userContinentCode.values()))
+    plt.xticks(continents, continents)
+    plt.show()
+#TASK2B END
+
+def task2A():
+    documentID = "131203154832-9b8594b7ec211f7e1a0782fd9883a42c"
+    #documentID = input("Enter the document ID: ") ------- UNCOMMENT ONCE FINISHED TESTING
+    countryCount(documentID)
+    countryPrint()
+
+def task2B():
+    documentID = "131203154832-9b8594b7ec211f7e1a0782fd9883a42c"
+    continentCount(documentID)
+    continentPrint()
 
 def main():
+    #Do this for all tasks
     fileLocation = input("Enter JSON datset file location: ")
     readJSON(fileLocation)
 
+    #task2A()
 
+    task2B()
 
 # CHECK IF THIS IS MAIN FILE
 if __name__ == "__main__":
